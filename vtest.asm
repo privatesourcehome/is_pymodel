7c496643c8867cdd28de85c4d6fd6fa7d1cc1862
privatesourcehome <102334719+privatesourcehome@users.noreply.github.com>
2022-05-24 17:38:27 +0900

    full modified

--- vtest.asm
+++ vtest.asm
@@ -1,13 +1,16 @@
 0 LOAD_V vr1 adr1
 1 LOAD_V vr2 adr2
 2 LOAD_V vr3 adr3
-3 MUL_VV vr4 vr1 vr2
-4 __DEBUG_ASSERT_VALUE 4
-5 MUL_VV vr5 vr1 vr3
-6 __DEBUG_ASSERT_VALUE 2
 # vr1 = [1, 0, 1]
 # vr2 = [1, 1, 3]
 # vr3 = [1, 0, 1]
+3 MUL_VV r1 vr1 vr2
+4 __DEBUG_ASSERT_SCALAR_VALUE r1 4
 # vr4 = [1*1 + 0*1 + 1*3] -> 4 (dot-product)
+5 MUL_VV r2 vr1 vr3
+6 __DEBUG_ASSERT_SCALAR_VALUE r2 2
 # vr5 = [1*1 + 0*0 + 1*1] -> 2 (dot-product)
-7 __DEBUG_EXIT
+7 LOAD_V vr4 adr4
+8 VReLU vr1 vr4
+9 __DEBUG_ASSERT_VECTOR_VALUE vr1 [1,0,3,0,5]
+10 __DEBUG_EXIT
