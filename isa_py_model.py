0c127e9fccf05fdf73faa84dc304752867330edd
privatesourcehome <shy.hooking001@gmail.com>
2022-06-22 16:45:52 +0900

    Revert "conv_test_fin"
    This reverts commit 36b4219d89821653a643b25682b8b03b1d72ad2b.

--- isa_py_model.py
+++ isa_py_model.py
@@ -3,12 +3,14 @@ import numpy as np
 MEM_SIZE=10000000
 comment = ('#','//') 
 reg = {'r1': 0, 'r2': 0, 'r3': 0, 'r4': 0,
-       'r5': 0, 'r6': 0, 'r7': 0, 'r8': 0, 'r9': 0, 'r10': 0, 'r11': 0, 'r12': 0, 'r13': 0, 'r14': 0, 'r15': 0, 'r16': 0, 'r17': 0, 'r18': 0, 'result': 0,'pc': 0, 'EXIT': False, 'EQ': False}
-vreg = {'vr0':0, 'vr1':0, 'vr2':0, 'vr3':0, 'vr4':0, 'vr5':0}
+       'r5': 0, 'r6': 0, 'r7': 0, 'r8': 0, 'r9': 0, 'r10': 0, 'r11': 0, 'r12': 0, 'r13': 0, 'r14': 0, 'r15': 0, 'r16': 0, 'r17': 0, 'r18': 0}
+spreg = {'result': 0,'pc': 0, 'EXIT': False, 'EQ': False}
+vreg = {'vr1':0, 'vr2':0, 'vr3':0, 'vr4':0, 'vr5':0}
 addr = {'adr1': 0, 'adr2': 1, 'adr3': 2, 'adr4': 3, 'adr5': 4}
 memory  = [0] * MEM_SIZE
 imemory = [0] * MEM_SIZE
 vmemory = [0] * MEM_SIZE
+kmemory = [0] * MEM_SIZE
 img = np.load('lena_gray.npy')
 for i in range(0, img.shape[0]):
     vmemory[i] = img[i]
@@ -30,7 +32,7 @@ for i in range(0, out.shape[0]):
     memory[7000000+i] = list(out[i])
 # Memory operation
 def LOAD_V(opr):
-    vreg[opr[0]] = vmemory[int(opr[1])]
+    vreg[opr[0]] = vmemory[opr[1]]
     reg['pc'] = reg['pc']+1
     '''
 def LOAD_V(opr):
@@ -38,7 +40,7 @@ def LOAD_V(opr):
     reg['pc'] = reg['pc']+1
     '''
 def LOAD_S(opr):
-    reg[opr[0]]=memory[int(opr[1])]
+    reg[opr[0]]=memory[reg[opr[1]]]
     reg['pc']=reg['pc']+1
 def LOAD_SI(opr):
     reg[opr[0]]=int(opr[1])
@@ -57,7 +59,7 @@ def STORE_V(opr):
     reg['pc']=reg['pc']+1
 def MOV_VS(opr):
     # vreg 지정, 몇번째 데이터, to reg
-    reg[opr[0]] = vreg[opr[1]][int(opr[2])]
+    reg[opr[0]] = vmemory[opr[1]][opr[2]]
     reg['pc'] = reg['pc']+1
 def MOV_SI(opr):
     reg[opr[0]] = int(opr[1])
@@ -112,8 +114,6 @@ def VReLU(opr):
     reg['pc'] = reg['pc']+1
 
 # Debug function
-def __DEBUG_PASS(opr):
-    reg['pc'] = reg['pc']+1  
 def __DEBUG_ASSERT_SCALAR_VALUE(opr):
     assert reg[opr[0]] == int(opr[1]) and __DEBUG_PRT_SUC_MSG(
         msg='__DEBUG_ASSERT_VALUE Test Success'), 'fail'
@@ -135,7 +135,6 @@ def __DEBUG_EXIT(opr):
 def __DEBUG_BREAK(opr):
     reg['pc'] = reg['pc']+1
 def __DEBUG_PRT_SUC_MSG(msg='success!'):
-    
     print(msg)
     return True
  
@@ -164,7 +163,7 @@ def __EXEC_ASM():
         op(imemory[i][1:])
         pass
 def __INIT_IMEM():
-    file = open('./conv_test.asm', 'r')
+    file = open('./vtest.asm', 'r')
     for line in file:
         if line.startswith(comment[:]):
             continue
